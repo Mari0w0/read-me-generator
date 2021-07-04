d9569ba2eaa8c868534c178cd69a56185ad8958e
Mari0w0 <marilynperalta002@gmail.com>
2021-07-04 10:12:36 -0400

    adds read me text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,13 +8,40 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `[MIT](https://choosealicense.com/licenses/mit/)`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description 
+${data.description}
+  
+## Table of Contents 
+​
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+​
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+
+  ## Installation
+
+
+  ## Contributing
+  Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+  
+  Please make sure to update tests as appropriate.
+  
+  ## License
+
+  ${renderLicenseSection(data.blahblah)}
+  `
 }
 
 module.exports = generateMarkdown;
